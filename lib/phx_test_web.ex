4b136cbcfa14ef8c8f590be08e7baceda7d554ea
Dallin Osmun <dosmun@gmail.com>
2020-05-26 12:42:16 -0600

    mix phx.gen.live Accounts User users name:string

--- lib/phx_test_web.ex
+++ lib/phx_test_web.ex
@@ -83,6 +83,7 @@ defmodule PhxTestWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import PhxTestWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
